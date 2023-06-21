9042fd6e51fe30f76f17d2b1adf9ca84f2f03acb
Suporte <Suporte@DESKTOP-H62CNE3>
2023-06-21 08:44:08 -0300

    Add project files.

---
+++ Api-Crud-Dotnet-Core/Controllers/TaskController.cs
@@ -0,0 +1,67 @@
+﻿using Api_Crud_Dotnet_Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Api_Crud_Dotnet_Core.Controllers
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private static List<Task> tasksList = new List<Task> ();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Task>> GetTasks ()
+        {
+            return tasksList;
+        }
+
+        [HttpGet ("{id}")]
+        public ActionResult<Task> GetTaskById ( long id )
+        {
+            var task = tasksList.Find (t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound ();
+            }
+            return task;
+        }
+
+        [HttpPost]
+        public ActionResult<Task> CreateTask ( Task task )
+        {
+            tasksList.Add (task);
+            return CreatedAtAction (nameof (GetTaskById), new { id = task.Id }, task);
+        }
+
+        [HttpPut ("{id}")]
+        public ActionResult<Task> UpdateTask ( long id, Task updatedTask )
+        {
+            var task = tasksList.Find (t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound ();
+            }
+
+            task.Name = updatedTask.Name;
+            task.Priority = updatedTask.Priority;
+            task.Finished = updatedTask.Finished;
+
+            return Ok (task);
+        }
+
+
+        [HttpDelete ("{id}")]
+        public ActionResult DeleteTask ( long id )
+        {
+            var task = tasksList.Find (t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound ();
+            }
+            tasksList.Remove (task);
+            return NoContent ();
+        }
+    }
+}
+
---
+++ Api-Crud-Dotnet-Core/Entities/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace Api_Crud_Dotnet_Core.Entities
+{
+    public class Task
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Priority { get; set; }
+        public bool Finished { get; set; }
+    }
+}
